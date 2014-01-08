dc18833266674d6fdff2a6ef2c9da60acda668f5
SweSnow <swesnowme@gmail.com>
2014-01-08 13:40:44 +0100

    News scrolls with user, styled video search input

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ $(".feed-video-title").hover(
     alert("nothover");
   }
 );
+
+window.addEventListener("scroll",function() { 
+   if(window.scrollY < 80) {
+   		var margin_cobo = '-' + window.scrollY.toString() + 'px';
+
+   		$('#news-scroll-container').css('margin-top', margin_cobo);
+   }
+   else {
+    	$('#news-scroll-container').css('margin-top', '-80px');
+   }
+},false);
